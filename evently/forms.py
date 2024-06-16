9707fde0af9e9c2a89d9b4a661f8cfc944586e1f
Roger <rogerszwaja23@proton.me>
2024-06-16 13:17:29 +0200

    Create User

--- evently/forms.py
+++ evently/forms.py
@@ -1,8 +1,16 @@
+from django.contrib.auth.forms import UserCreationForm
 from django.forms import ModelForm
 
-from .models import Event
+from .models import Event, CreateUserModel
+
 
 class EventForm(ModelForm):
     class Meta:
         model = Event
-        fields = ['name', 'place', 'start_at', 'end_at', 'description']
+        fields = ['name', 'place', 'start_at', 'end_at', 'description', ]
+
+
+class CreateUserForm(UserCreationForm):
+    class Meta:
+        model = CreateUserModel
+        fields = ['first_name', 'last_name', 'username', 'email', ]
