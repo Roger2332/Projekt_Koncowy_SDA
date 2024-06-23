06369c448e0197bd1cfd2e84a937c9a2c3c020d3
Roger <rogerszwaja23@proton.me>
2024-06-23 11:08:09 +0200

    repair click the filter and add an event

--- evently/static/js/add_event.js
+++ evently/static/js/add_event.js
@@ -1,8 +1,12 @@
-        document.getElementById('event_form').addEventListener('click', function() {
-            var addEventDiv = document.getElementById('add_event_new');
-            if (addEventDiv.classList.contains('hidden')) {
-                addEventDiv.classList.remove('hidden');
-            } else {
-                addEventDiv.classList.add('hidden');
-            }
-        });
+document.addEventListener("DOMContentLoaded", function() {
+    var toggleAddEventButton = document.getElementById("toggleAddEventButton");
+    var addEventForm = document.getElementById("addEventForm");
+
+    toggleAddEventButton.addEventListener("click", function() {
+        if (addEventForm.style.display === "none" || addEventForm.style.display === "") {
+            addEventForm.style.display = "block";
+        } else {
+            addEventForm.style.display = "none";
+        }
+    });
+});
