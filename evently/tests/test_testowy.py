50baf0daa0e2fd08bf205209d6dfa6acc52b6cc0
Roger <rogerszwaja23@proton.me>
2024-06-24 21:18:58 +0200

    add pytest

---
+++ evently/tests/test_testowy.py
@@ -0,0 +1,6 @@
+import pytest
+
+@pytest.mark.django_db
+def test_event_creation():
+
+    assert False == False
---
+++ evently/tests/tests_testu2.py
@@ -0,0 +1,7 @@
+import pytest
+
+@pytest.mark.django_db
+def test_event_creation():
+
+    assert True == True
+
