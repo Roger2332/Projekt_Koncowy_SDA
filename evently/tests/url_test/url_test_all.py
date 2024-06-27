eaf67cd0ff42bde8ae12da5f36d6b293ad4e3c7d
Roger <rogerszwaja23@proton.me>
2024-06-27 16:22:39 +0200

    add ALL test url

--- evently/tests/url_test/url_test_all.py
+++ evently/tests/url_test/url_test_all.py
@@ -14,57 +14,103 @@ def client():
 
 
 @pytest.fixture
+def admin():
+    return CreateUserModel.objects.create_superuser(username='admin', email='admin@gmai.com', password='admin12345')
+
+
+@pytest.fixture
 def user():
     users = CreateUserModel.objects.create_user(username='testuser', email='user1@gmail.com', password='12345')
     return users
 
 
-
-@pytest.fixture
-def sample_categories():
-    # Tworzenie przykładowych obiektów Category do testów
-    category1 = Category.objects.create(name='Kategoria 1')
-    category2 = Category.objects.create(name='Kategoria 2')
-    return [category1, category2]
-
-
 @pytest.fixture
 def sample_statuses():
     status1 = Status.objects.create(name='Active')
-    status2 = Status.objects.create(name='Inactive')
-    return (status1, status2)
+    return status1
+
+
 @pytest.fixture
 def sample_events(user, sample_statuses):
     # Tworzenie przykładowych obiektów Event do testów
     event1 = Event.objects.create(name='Wydarzenie 1', place='Miejsce A', start_at=date(2024, 7, 1),
-                                  end_at=date(2024, 7, 2), author=user, status_id=sample_statuses[0].id)
+                                  end_at=date(2024, 7, 2), author=user, status_id=sample_statuses.id)
     return event1
 
 
+@pytest.mark.django_db
+def test_admin_url_by_the_admin(client, admin):
+    logged_in = client.login(username='admin', password='admin12345')
+    assert logged_in
+    response = client.get('/admin/')
+    assert response.status_code == 200
+
 
 @pytest.mark.django_db
-def test_admin_url(client):
-    response = client.get('/admin')
-    assert response.status_code == 301
+def test_admin_url_by_the_user(client, user):
+    logged_in = client.login(username='testuser', password='12345')
+    assert logged_in
+    response = client.get('/admin/')
+    assert response.status_code == 302
+    assert '/admin/login/' in response.url
 
 
 @pytest.mark.django_db
-def test_create_category_url(client, user):
+def test_create_category_url_by_the_user(client, user):
     client.login(username='testuser', password='12345')
     response = client.get(reverse('create_category'))
     assert response.status_code == 403
 
 
 @pytest.mark.django_db
-def test_accept_status_url(client):
-    response = client.get(reverse('accept_status'))
-    assert response.status_code == 302
+def test_create_category_url_by_the_admin(client, admin):
+    client.login(username='admin', password='admin12345')
+    response = client.get(reverse('create_category'))
+    assert response.status_code == 200
 
 
 @pytest.mark.django_db
-def test_update_event_status_url(client):
+def test_accept_status_url_no_user(client):
+    response = client.get(reverse('accept_status'))
+    assert response.status_code == 302
+    assert 'accounts/login' in response.url
+
+
+@pytest.mark.django_db
+def test_accept_status_url_by_the_user(client, user):
+    client.login(username='testuser', password='12345')
+    response = client.get(reverse('accept_status'))
+    assert response.status_code == 302
+    assert 'accounts/login' in response.url
+
+
+@pytest.mark.django_db
+def test_accept_status_url_by_the_admin(client, admin):
+    client.login(username='admin', password='admin12345')
+    response = client.get(reverse('accept_status'))
+    assert response.status_code == 200
+
+
+@pytest.mark.django_db
+def test_update_event_status_url_no_user(client):
     response = client.get(reverse('update_event_status'))
     assert response.status_code == 302
+    assert 'accounts/login' in response.url
+
+
+@pytest.mark.django_db
+def test_update_event_status_url_by_the_admin(client, admin):
+    client.login(username='admin', password='admin12345')
+    response = client.get(reverse('update_event_status'))
+    assert response.status_code == 200
+
+
+@pytest.mark.django_db
+def test_update_event_status_url_by_the_user(client, user):
+    client.login(username='testuser', password='12345')
+    response = client.get(reverse('update_event_status'))
+    assert response.status_code == 302
+    assert 'accounts/login' in response.url
 
 
 @pytest.mark.django_db
@@ -98,6 +144,7 @@ def test_edit_event_url(client, sample_events, user):
     response = client.get(reverse('event_edit', args=[sample_events.pk]))
     assert response.status_code == 200
 
+
 @pytest.mark.django_db
 def test_delete_event_authenticated_author(client, user, sample_events):
     client.login(username='testuser', password='12345')
@@ -107,7 +154,9 @@ def test_delete_event_authenticated_author(client, user, sample_events):
     # Usunięcie wydarzenia
     response = client.post(reverse('delete_event', args=[sample_events.pk]))
     assert response.status_code == 302  # Przekierowanie po usunięciu
+    assert 'list_events' in response.url
     assert not Event.objects.filter(pk=sample_events.pk).exists()  # Sprawdzenie, że wydarzenie zostało usunięte
+
 
 @pytest.mark.django_db
 def test_delete_event_authenticated_not_author(client, user, sample_events):
@@ -117,10 +166,12 @@ def test_delete_event_authenticated_not_author(client, user, sample_events):
     response = client.get(reverse('delete_event', args=[sample_events.pk]))
     assert response.status_code == 403  # Użytkownik nie jest autorem, więc otrzymujemy Forbidden
 
+
 @pytest.mark.django_db
 def test_delete_event_not_authenticated(client, sample_events):
     response = client.get(reverse('delete_event', args=[sample_events.pk]))
     assert response.status_code == 302  # Użytkownik nie jest zalogowany, więc otrzymujemy Forbidden
+    assert 'accounts/login' in response.url
 
 
 @pytest.mark.django_db
@@ -130,14 +181,20 @@ def test_detail_event_url(client, sample_events):
 
 
 @pytest.mark.django_db
-def test_subscribe_event_url(client, sample_events, user):
+def test_subscribe_event_url_by_user(client, sample_events, user):
     client.login(username='testuser', password='12345')
     login_response = client.get(reverse('login'))
     csrf_token = login_response.cookies['csrftoken'].value
     response = client.post(reverse('subscribe_event', args=[sample_events.pk]), {'csrfmiddlewaretoken': csrf_token})
     assert response.status_code == 302
-
     assert response.url == reverse('detail_event', args=[sample_events.pk])
+
+
+@pytest.mark.django_db
+def test_subscribe_event_url_no_user(client, sample_events):
+    response = client.post(reverse('subscribe_event', args=[sample_events.pk]))
+    assert response.status_code == 302
+    assert 'accounts/login' in response.url
 
 
 @pytest.mark.django_db
@@ -173,7 +230,7 @@ def test_logout_view(client, user):
     # Sprawdzenie, czy użytkownik został wylogowany
     response = client.get(reverse('user_profile'))
     assert response.status_code == 302
-
+    assert 'accounts/login' in response.url
 
 
 @pytest.mark.django_db
@@ -188,7 +245,8 @@ def test_user_events_url(client, sample_events, user):
     client.login(username='testuser', password='12345')
     response = client.get(reverse('user_events', args=[sample_events.pk]))
     assert response.status_code == 302
-    assert response.url == reverse('search_event') + '?query=&search_type=all&place=&category=&organizer=1&start_date=&end_date='
+    assert response.url == reverse(
+        'search_event') + '?query=&search_type=all&place=&category=&organizer=1&start_date=&end_date='
 
 
 @pytest.mark.django_db
@@ -199,15 +257,35 @@ def test_user_subscriptions_url(client, user):
 
 
 @pytest.mark.django_db
-def test_password_change_url(client):
+def test_password_change_url_no_user(client):
     response = client.get(reverse('password_change'))
     assert response.status_code == 302
+    assert 'accounts/login' in response.url
 
 
 @pytest.mark.django_db
-def test_password_change_done_url(client):
+def test_password_change_url(client, user):
+    client.login(username='testuser', password='12345')
+    login_response = client.get(reverse('login'))
+    response = client.get(reverse('password_change'))
+    assert response.status_code == 200
+
+
+
+@pytest.mark.django_db
+def test_password_change_done_url_no_user(client):
     response = client.get(reverse('password_change_done'))
     assert response.status_code == 302
+    assert 'accounts/login' in response.url
+
+@pytest.mark.django_db
+def test_password_change_done_url_by_user(client):
+    client.login(username='testuser', password='12345')
+    login_response = client.get(reverse('login'))
+    response = client.get(reverse('password_change_done'))
+    assert response.status_code == 302
+    assert 'accounts/login' in response.url
+
 
 
 @pytest.mark.django_db
@@ -226,6 +304,7 @@ def test_password_reset_done_url(client):
 def test_password_reset_confirm_url(client):
     response = client.get(reverse('password_reset_confirm', args=['uidb64', 'token']))
     assert response.status_code == 200
+
 
 
 @pytest.mark.django_db
