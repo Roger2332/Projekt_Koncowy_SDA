0474292b35fd3e83b39f441323672c578731ee2a
Roger <rogerszwaja23@proton.me>
2024-07-26 14:41:37 +0200

    add testing podgres

--- Projekt_Koncowy/settings.py
+++ Projekt_Koncowy/settings.py
@@ -73,23 +73,23 @@ WSGI_APPLICATION = 'Projekt_Koncowy.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.mysql",
-        "NAME": "evently",
-        "USER": "evently",
-        "PASSWORD": "evently123",
-        "HOST": "db",
-        "PORT": "3306",
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.postgresql",
+#         "NAME": "evently",
+#         "USER": "evently",
+#         "PASSWORD": "evently123",
+#         "HOST": "db",
+#         "PORT": "5432",
+#     }
+# }
 
 
 
